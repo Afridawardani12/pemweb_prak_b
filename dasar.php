89fa570a0c54345dad5916067db8889b81f2e46b
Afridawardani12 <wardaniafrida@gmail.com>
2021-03-10 20:16:12 +0700

    pertemuan ke 1

---
+++ dasar.php
@@ -0,0 +1,36 @@
+<?php
+    echo "Hello word:";
+
+
+foreach ($array as $value) {
+    echo $value . " ;
+}
+// berguna untuk melakukan loop seusai yang diinginkan//
+for ($i = 0; $i < count($array); $i++) {
+
+}
+?>
+<!Doc typefile HTML>
+<html>
+<head>
+	<title>.</title>
+</head>
+<body>
+<h1>Belajar PHP</h1>
+<?php
+
+$a = 10;
+$b = 4;
+$c = $a + $b; 
+echo "Hasil dari Tambahan $a + $b = $c"; 
+echo "</br>"; 
+echo "=======================";
+echo "</br>";
+
+$x = 7; 
+$y = 4;
+$z = $x - $y;
+echo "Hasil dari Pengurangan $x - $y = $z"; 
+?>
+</body>
+</html>
